4cf6307102c2e8da5f6d6baea6d8323c182d6436
savan.awanti <savan.awanti@HMECMB00333.local>
2023-05-16 16:53:43 +0530

    migration file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :books do 
+    resources :reviews
+  end
+ root "book#index"
 end
--- db/migrate/20230516110024_create_reviews.rb
+++ db/migrate/20230516110024_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.string :comment
+      t.belongs_to :book
 
       t.timestamps
     end

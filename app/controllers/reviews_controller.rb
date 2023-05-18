dd360135df84b136c23c738c35b17b802b2b3245
savan.awanti <savan.awanti@HMECMB00333.local>
2023-05-18 10:07:45 +0530

    navbar and before_action done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
   # GET /reviews or /reviews.json
   def index
     @books=Book.find_by(params[:book_id])

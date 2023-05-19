adf07e538901f32c35a17a503bb430b00b4c12c8
savan.awanti <savan.awanti@HMECMB00333.local>
2023-05-19 13:24:32 +0530

    css modification and security to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def authenticate_admin!
+  if current_user && current_user.is_admin
+    # fine
+  else
+    redirect_to new_user_session_path , alert: "it is for admin users"
+  end
+end
 end
